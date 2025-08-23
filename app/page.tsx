5afafe7afaeb9a90b0f385719654e41a09672306
Nenad Marinković <nenadmarinkovic@protonmail.com>
2025-08-24 00:56:25 +0200

    wrap in suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,26 @@
 import { CategorySidebar } from '@/components/CategorySidebar';
 import { Header } from '@/components/Header';
-import { WebsiteList } from '@/components/WebsiteList'; // Import the new component
+import { WebsiteList } from '@/components/WebsiteList';
 import { Suspense } from 'react';
 
-// This component no longer needs to be async
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <div className="flex flex-1">
-        <CategorySidebar />
+        <Suspense
+          fallback={
+            <aside className="p-4 w-48 border-r">
+              Loading categories...
+            </aside>
+          }
+        >
+          <CategorySidebar />
+        </Suspense>
         <main className="flex-1">
-          {/* Use React Suspense as a best practice for loading client components */}
-          <Suspense fallback={<p>Loading...</p>}>
+          <Suspense
+            fallback={<p className="p-4">Loading websites...</p>}
+          >
             <WebsiteList />
           </Suspense>
         </main>
