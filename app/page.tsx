f42dc84ffdd51a324f93d99a3e7b0dc6c1c4d013
Nenad Marinković <nenadmarinkovic@protonmail.com>
2025-09-22 14:57:51 +0200

    add skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,16 @@
-import { Suspense } from 'react';
-import dynamic from 'next/dynamic';
 import { Header } from '@/components/Header';
 import { CategorySidebar } from '@/components/CategorySidebar';
-
-const WebsiteList = dynamic(() => import('@/components/WebsiteList').then((mod) => mod.WebsiteList));
+import { WebsiteList } from '@/components/WebsiteList';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <div className="flex flex-1">
-        <Suspense
-          fallback={
-            <aside className="p-4 w-48 border-r">
-              Loading categories...
-            </aside>
-          }
-        >
-          <CategorySidebar />
-        </Suspense>
+
+        <CategorySidebar />
         <main className="flex-1 bg-[#fcfcfc] dark:bg-black min-h-screen">
-          <Suspense
-            fallback={<p className="p-4">Loading websites...</p>}
-          >
-            <WebsiteList />
-          </Suspense>
+          <WebsiteList />
         </main>
       </div>
     </div>
