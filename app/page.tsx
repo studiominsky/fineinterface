2e1a6a6c87983dfe7c23e93fa152fb1fc9d629e5
Nenad Marinković <nenadmarinkovic@protonmail.com>
2025-09-23 10:07:32 +0200

    add suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
+import { Suspense } from 'react';
 import { Header } from '@/components/Header';
 import { CategorySidebar } from '@/components/CategorySidebar';
 import { WebsiteList } from '@/components/WebsiteList';
+import Loading from '@/components/Loading';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <div className="flex flex-1">
-        <CategorySidebar />
-        <main className="flex-1 bg-[#fcfcfc] dark:bg-black min-h-screen">
-          <WebsiteList />
-        </main>
-
+        <Suspense fallback={<Loading />}>
+          <CategorySidebar />
+          <main className="flex-1 bg-[#fcfcfc] dark:bg-black min-h-screen">
+            <WebsiteList />
+          </main>
+        </Suspense>
       </div>
     </div>
   );
